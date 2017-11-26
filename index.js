ed362cd44049429ae6612164aaf6522c7362204d
Thomas Jascur <tjascur@yahoo.com>
2017-11-26 18:18:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Code your solution in this file!
 const companyName = 'Scuber';
 let mostProfitableNeighborhood = 'Chelsea'
+<<<<<<< HEAD
 let companyCeo = 'Susan Smith'
+=======
+let companyCeo = 'Susan Smith'
+>>>>>>> 985c0c67bae3233e3c46eb6484e8f0764fe3000d
